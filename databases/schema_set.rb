d2e9d5f60de58f8ddf645af63a8415dbba0c1ce2
Peter Donald <peter@realityforge.org>
2010-11-09 21:59:29 +1100

    Decruft

--- databases/schema_set.rb
+++ databases/schema_set.rb
@@ -4,8 +4,6 @@ Domgen.define_schema_set(:footprints) do |ss|
   ss.define_generator(:jpa_model)
   ss.define_generator(:jpa_ejb)
 
-  ss.jpa.provider = "org.hibernate.ejb.HibernatePersistence"
-
   ss.define_schema('JavaNCSS') do |s|
     s.sql.schema = 'JavaNCSS'
     s.java.package = 'footprints.javancss.model'
