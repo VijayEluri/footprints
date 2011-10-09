41807be9544c525b6f2e8e8b43b345ed812106b9
Peter Donald <peter@realityforge.org>
2011-10-09 15:25:31 +1100

    Separate out testing guff into a separate data_module. Start to add imit facet ito mix

--- databases/schema_set.rb
+++ databases/schema_set.rb
@@ -3,9 +3,50 @@ Domgen.repository(:footprints) do |repository|
   repository.enable_facet(:jpa)
   repository.enable_facet(:ruby)
   repository.enable_facet(:ejb)
+  repository.enable_facet(:imit)
 
-  repository.data_module(:JavaNCSS) do |data_module|
+  repository.data_module(:CodeMetrics) do |data_module|
     data_module.jpa.entity_package = 'footprints.javancss.model'
+    data_module.imit.imitation_package = 'footprints.javancss.imit'
+
+    data_module.entity(:Collection) do |t|
+      t.integer(:ID, :primary_key => true)
+      t.datetime(:CollectedAt, :immutable => true)
+    end
+
+    data_module.entity(:MethodMetric) do |t|
+      t.integer(:ID, :primary_key => true)
+      t.reference(:Collection, :immutable => true) do |a|
+        a.inverse.traversable = true
+      end
+      t.string(:PackageName, 500, :immutable => true)
+      t.string(:ClassName, 500, :immutable => true)
+      t.string(:MethodName, 500, :immutable => true)
+
+      # Non commenting source statements
+      t.integer(:NCSS, :immutable => true)
+
+      #Cyclomatic complexity
+      t.integer(:CCN, :immutable => true)
+
+      #Javadoc comments
+      t.integer(:JVDC, :immutable => true)
+    end
+  end
+
+  repository.data_module(:BaseTestModule) do |data_module|
+    data_module.jpa.entity_package = 'footprints.base_tester.model'
+    data_module.imit.imitation_package = 'footprints.base_tester.imit'
+
+    data_module.entity(:BaseX, :final => false) do |t|
+      t.integer(:ID, :primary_key => true)
+    end
+  end
+
+
+  repository.data_module(:TestModule) do |data_module|
+    data_module.jpa.entity_package = 'footprints.tester.model'
+    data_module.imit.imitation_package = 'footprints.tester.imit'
 
     data_module.service(:Collector) do |s|
 
@@ -32,30 +73,6 @@ Domgen.repository(:footprints) do |repository|
       s.method(:DoStuff) do |m|
         m.exception(:Foo)
       end
-    end
-
-    data_module.entity(:Collection) do |t|
-      t.integer(:ID, :primary_key => true)
-      t.datetime(:CollectedAt, :immutable => true)
-    end
-
-    data_module.entity(:MethodMetric) do |t|
-      t.integer(:ID, :primary_key => true)
-      t.reference(:Collection, :immutable => true) do |a|
-        a.inverse.traversable = true
-      end
-      t.string(:PackageName, 500, :immutable => true)
-      t.string(:ClassName, 500, :immutable => true)
-      t.string(:MethodName, 500, :immutable => true)
-
-      # Non commenting source statements
-      t.integer(:NCSS, :immutable => true)
-
-      #Cyclomatic complexity
-      t.integer(:CCN, :immutable => true)
-
-      #Javadoc comments
-      t.integer(:JVDC, :immutable => true)
     end
 
     data_module.entity(:Foo) do |t|
@@ -100,31 +117,32 @@ Domgen.repository(:footprints) do |repository|
       t.relationship_constraint(:gte, :A, :B)
       t.relationship_constraint(:lte, :A, :B)
       t.relationship_constraint(:lte, :C, :D)
-      t.i_enum(:LinkType, {"URL" => 0, "JAVA" => 1}) do |a|
+      t.i_enum(:LinkType, { "URL" => 0, "JAVA" => 1 }) do |a|
         a.description(<<TEXT)
   The type of the link.
 
   * A link that starts an external browser using the URL derived from the Target attribute.
   * A link that invokes some java code with the class name specified in the Target attribute.
 TEXT
-        t.s_enum(:ElementType, {"PhysicalUnit" => "PhysicalUnit",
-                                "Crew" => "Crew",
-                                "RoleType" => "RoleType",
-                                "SpecificTask" => "SpecificTask",
-                                "TemplateTask" => "TemplateTask",
-                                "ManagementProject" => "ManagementProject",
-                                "TaskClassification" => "TaskClassification",
-                                "Classification" => "Classification"})
+        t.s_enum(:ElementType, { "PhysicalUnit" => "PhysicalUnit",
+                                 "Crew" => "Crew",
+                                 "RoleType" => "RoleType",
+                                 "SpecificTask" => "SpecificTask",
+                                 "TemplateTask" => "TemplateTask",
+                                 "ManagementProject" => "ManagementProject",
+                                 "TaskClassification" => "TaskClassification",
+                                 "Classification" => "Classification" })
       end
 
     end
 
-    data_module.entity(:BaseX, :final => false) do |t|
-      t.integer(:ID, :primary_key => true)
-    end
-
-    data_module.entity(:ExtendedX, :extends => :BaseX) do |t|
+    data_module.entity(:ExtendedX, :extends => :"BaseTestModule.BaseX", :final => false) do |t|
       t.string(:Name, 50, :immutable => true)
     end
+
+    data_module.entity(:ExtendedExtendedX, :extends => :ExtendedX) do |t|
+      t.string(:Description, 50, :immutable => true)
+    end
   end
+
 end
