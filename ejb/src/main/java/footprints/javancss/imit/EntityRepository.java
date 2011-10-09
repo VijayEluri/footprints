5b9da8c46cc43bbb570d3e28eb1031ee22dd40f8
Peter Donald <peter@realityforge.org>
2011-10-09 22:03:51 +1100

    Update the repository to support forced loadig of entities and deregisterig entities

--- ejb/src/main/java/footprints/javancss/imit/EntityRepository.java
+++ ejb/src/main/java/footprints/javancss/imit/EntityRepository.java
@@ -1,8 +1,18 @@
 package footprints.javancss.imit;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 public interface EntityRepository
 {
-  <T> T registerEntity( Class<T> type, Object id, Object entity );
+  <T> void registerEntity( Class<T> type, Object id, T entity );
 
+  @Nonnull
+  <T> T deregisterEntity( Class<T> type, Object id );
+
+  @Nonnull
+  <T> T getByID( Class<T> type, Object id );
+
+  @Nullable
   <T> T findByID( Class<T> type, Object id );
 }
