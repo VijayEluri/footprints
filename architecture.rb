de5c282d9629742ac84b76f9a0fd7a327dae15b4
Peter Donald <peter@realityforge.org>
2012-06-08 11:48:42 +1000

    Update to the latest version of domgen

--- architecture.rb
+++ architecture.rb
@@ -105,20 +105,7 @@ Domgen.repository(:Footprints) do |repository|
 
       t.query('LookAtMe2') do |q|
         q.description("Some reason to do stuff")
-        q.integer(:Foo, :nullable => true)
-        q.s_enum(:ElementType, { "PhysicalUnit" => "PhysicalUnit",
-                                 "Crew" => "Crew",
-                                 "RoleType" => "RoleType",
-                                 "SpecificTask" => "SpecificTask",
-                                 "TemplateTask" => "TemplateTask",
-                                 "ManagementProject" => "ManagementProject",
-                                 "TaskClassification" => "TaskClassification",
-                                 "Classification" => "Classification" })
-
-      end
-
-      t.jpa.query('LookAtMe', "O.ID = :Foo OR O.ID = :ID OR :ElementType = '22'") do |q|
-        q.description("Some reason to do stuff")
+        q.jpa.jpql = "O.ID = :Foo OR O.ID = :ID OR :ElementType = '22'"
         q.integer(:Foo, :nullable => true)
         q.s_enum(:ElementType, { "PhysicalUnit" => "PhysicalUnit",
                                  "Crew" => "Crew",
