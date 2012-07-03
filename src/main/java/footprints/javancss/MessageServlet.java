0297fef6f1554aa7f3aaa34d109126a5201c92e4
Peter Donald <peter@realityforge.org>
2012-07-04 09:50:49 +1000

    Add some test code around MDBs

---
+++ src/main/java/footprints/javancss/MessageServlet.java
@@ -0,0 +1,57 @@
+package footprints.javancss;
+
+import java.io.IOException;
+import javax.annotation.Resource;
+import javax.jms.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet( urlPatterns = "/MessageServlet" )
+public class MessageServlet
+  extends HttpServlet
+{
+  @Resource( mappedName = "testQueue2" )
+  private Queue queue;
+
+  @Resource( mappedName = "jms/QueueConnectionFactory" )
+  private QueueConnectionFactory queueConnectionFactory;
+
+  @Override
+  protected void doGet( HttpServletRequest request, HttpServletResponse response )
+    throws ServletException, IOException
+  {
+    QueueConnection queueConnection = null;
+    try
+    {
+      queueConnection = queueConnectionFactory.createQueueConnection();
+      queueConnection.start();
+      QueueSession queueSession = queueConnection.createQueueSession( false,
+                                                                      Session.AUTO_ACKNOWLEDGE );
+      QueueSender sender = queueSession.createSender( queue );
+
+      TextMessage msg = queueSession.createTextMessage();
+      msg.setText( "A message from MessageServlet" );
+      msg.setStringProperty( "name", "MessageServlet" );
+
+      sender.send( msg );
+    }
+    catch ( JMSException e )
+    {
+      throw new RuntimeException( e );
+    }
+    finally
+    {
+      try
+      {
+        if ( queueConnection != null )
+        {
+          queueConnection.close();
+        }
+      }
+      catch ( JMSException e )
+      { //ignore
+      }
+    }
+  }
+}
