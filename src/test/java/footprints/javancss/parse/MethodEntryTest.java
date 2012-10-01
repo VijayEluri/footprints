aa5b23e1296ee55ee46b2718de17c701f6aca0ff
Peter Donald <peter@realityforge.org>
2012-10-01 10:51:42 +1000

    Add dummy test so that we have a test phase

---
+++ src/test/java/footprints/javancss/parse/MethodEntryTest.java
@@ -0,0 +1,13 @@
+package footprints.javancss.parse;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class MethodEntryTest
+{
+  @Test
+  public void x()
+  {
+    assertTrue( true );
+  }
+}
