6e73ab3a318b2c556a05102fa7da51ce3b255100
Peter Donald <peter@realityforge.org>
2012-12-20 12:54:17 +1100

    Add script to generate jms config

---
+++ config/jms_setup.sh
@@ -0,0 +1,2 @@
+asadmin create-jms-resource --restype javax.jms.Queue jms/TestModule.Collector.Subscribe
+asadmin create-jms-resource --restype javax.jms.QueueConnectionFactory jms/QueueConnectionFactory
