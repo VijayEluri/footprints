2b1469df67915fe24edbf7317c6c9044812771ba
Peter Donald <peter@realityforge.org>
2013-03-13 11:21:52 +1100

    Use fully qualified names of jaxrs elements

--- src/main/java/footprints/javancss/RestService.java
+++ src/main/java/footprints/javancss/RestService.java
@@ -3,28 +3,21 @@ package footprints.javancss;
 import footprints.server.data_type.code_metrics.CollectionDTO;
 import footprints.server.service.code_metrics.FormatErrorException;
 import java.util.List;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-@Path( "/collections" )
-@Produces( { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML } )
+@javax.ws.rs.Path( "/collections" )
+@javax.ws.rs.Produces( { javax.ws.rs.core.MediaType.APPLICATION_JSON, javax.ws.rs.core.MediaType.APPLICATION_XML } )
 public interface RestService
 {
-  @Consumes( MediaType.APPLICATION_XML )
-  @PUT
+  @javax.ws.rs.Consumes( javax.ws.rs.core.MediaType.APPLICATION_XML )
+  @javax.ws.rs.PUT
   void uploadCollection( String content )
     throws FormatErrorException;
 
-  @GET
+  @javax.ws.rs.GET
   List<CollectionDTO> getCollections();
 
-  @GET
-  @Produces( MediaType.APPLICATION_JSON )
-  @Path( "/{id}" )
-  CollectionDTO getCollection( @PathParam( "id" ) int id );
+  @javax.ws.rs.GET
+  @javax.ws.rs.Produces( javax.ws.rs.core.MediaType.APPLICATION_JSON )
+  @javax.ws.rs.Path( "/{id}" )
+  CollectionDTO getCollection( @javax.ws.rs.PathParam( "id" ) int id );
 }
