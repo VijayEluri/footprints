4218362daad83db18b8b4fdb8dc6d2cc83b67717
Peter Donald <peter@realityforge.org>
2013-05-05 12:39:12 +1000

    Add script to add spatial features to database

---
+++ config/ps_.sh
@@ -0,0 +1,2 @@
+psql -d peter_Footprints_DEV -f /usr/local/Cellar/postgis/2.0.2/share/postgis/postgis.sql
+psql -d peter_Footprints_DEV -f /usr/local/Cellar/postgis/2.0.2/share/postgis/spatial_ref_sys.sql
