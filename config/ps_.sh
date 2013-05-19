c113a04818790e015440069f2df880e84cb82169
Peter Donald <peter@realityforge.org>
2013-05-19 20:26:44 +1000

    Remove unneeded script

--- config/ps_.sh
+++
@@ -1,2 +0,0 @@
-psql -d peter_Footprints_DEV -f /usr/local/Cellar/postgis/2.0.2/share/postgis/postgis.sql
-psql -d peter_Footprints_DEV -f /usr/local/Cellar/postgis/2.0.2/share/postgis/spatial_ref_sys.sql
