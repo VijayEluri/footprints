e6d1b8d4c2e17f660455cfc85a3d7235d5419a6c
Peter Donald <peter@realityforge.org>
2013-07-10 20:56:59 +1000

    format code style

--- src/main/java/footprints/client/service/code_metrics/BaseFormatErrorException.java
+++ src/main/java/footprints/client/service/code_metrics/BaseFormatErrorException.java
@@ -1,26 +1,36 @@
 package footprints.client.service.code_metrics;
 
-@javax.annotation.Generated( "Domgen" )
-@SuppressWarnings( { "UnusedDeclaration", "JavaDoc" } )
+@javax.annotation.Generated("Domgen")
+@SuppressWarnings({ "UnusedDeclaration", "JavaDoc" })
 public class BaseFormatErrorException
   extends Exception
 {
+  @javax.annotation.Nonnull
+  private String File;
 
-  private @javax.annotation.Nonnull String File;
+  public BaseFormatErrorException( final @javax.annotation.Nonnull String File )
+  {
+    this( File, null, null );
+  }
 
-  public BaseFormatErrorException( final @javax.annotation.Nonnull String File ) { this( File, null, null ); }
-  public BaseFormatErrorException( final @javax.annotation.Nonnull String File, final String message ) { this( File, message, null ); }
+  public BaseFormatErrorException( final @javax.annotation.Nonnull String File, final String message )
+  {
+    this( File, message, null );
+  }
+
   public BaseFormatErrorException( final @javax.annotation.Nonnull String File,
                                    final String message,
                                    final Throwable cause )
   {
     super( message, cause );
-      this.File = File;
+    this.File = File;
   }
 
 
-public @javax.annotation.Nonnull String getFile()
+  public
+  @javax.annotation.Nonnull
+  String getFile()
   {
-  return File;
-}
+    return File;
   }
+}
