d7d25674b2d988ddd24cad291a84b3ce66980b0b
Peter Donald <peter@realityforge.org>
2013-08-09 22:46:05 +1000

    Add global async callback for rpc

---
+++ src/main/java/footprints/client/GlobalAsyncCallback.java
@@ -0,0 +1,22 @@
+package footprints.client;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * Callback invoked on completion of every job
+ */
+public class GlobalAsyncCallback
+  implements AsyncCallback
+{
+  @Override
+  public void onFailure( final Throwable caught )
+  {
+    Window.alert( "Failed!" );
+  }
+
+  @Override
+  public void onSuccess( final Object result )
+  {
+  }
+}
