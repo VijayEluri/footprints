c195c7e6aed1ca86997fab42e8f8971fcc03ffae
Peter Donald <peter@realityforge.org>
2013-11-27 14:58:20 +1100

    Remove unused mappers

--- src/main/java/footprints/server/JaxRsActivator.java
+++ src/main/java/footprints/server/JaxRsActivator.java
@@ -1,19 +1,10 @@
 package footprints.server;
 
 import footprints.server.service.FootprintsJaxRsApplication;
-import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 
 @ApplicationPath( FootprintsJaxRsApplication.APPLICATION_PATH )
 public class JaxRsActivator
   extends FootprintsJaxRsApplication
 {
-  @Override
-  public Set<Class<?>> getClasses()
-  {
-    final Set<Class<?>> classes = super.getClasses();
-    classes.add( NoResultExceptionMapper.class );
-    classes.add( FormatErrorExceptionMapper.class );
-    return classes;
-  }
 }
