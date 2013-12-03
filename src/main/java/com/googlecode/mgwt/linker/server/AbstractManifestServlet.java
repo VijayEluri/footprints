0e4bfed68713335a46babde393037bd3a2b05fdc
Peter Donald <peter@realityforge.org>
2013-12-03 12:07:31 +1100

    Improve caching headers

--- src/main/java/com/googlecode/mgwt/linker/server/AbstractManifestServlet.java
+++ src/main/java/com/googlecode/mgwt/linker/server/AbstractManifestServlet.java
@@ -131,9 +131,17 @@ public abstract class AbstractManifestServlet
   public void serveStringManifest( final HttpServletResponse resp, final String manifest )
     throws ServletException
   {
-    resp.setHeader( "Cache-Control", "no-cache" );
+    final Date now = new Date();
+    // set create date to current timestamp
+    resp.setDateHeader( "Date", now.getTime() );
+    // set modify date to current timestamp
+    resp.setDateHeader( "Last-Modified", now.getTime() );
+    // set expiry to back in the past (makes us a bad candidate for caching)
+    resp.setDateHeader( "Expires", 0 );
+    // HTTP 1.1 (disable caching of any kind)
+    // HTTP 1.1 'pre-check=0, post-check=0' => (Internet Explorer should always check)
+    resp.setHeader( "Cache-control", "no-cache, no-store, must-revalidate, pre-check=0, post-check=0" );
     resp.setHeader( "Pragma", "no-cache" );
-    resp.setDateHeader( "Expires", new Date().getTime() );
 
     resp.setContentType( "text/cache-manifest" );
 
