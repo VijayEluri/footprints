233194dbd8e9e55b4dfd68172a1592ac1a33e1c4
Peter Donald <peter@realityforge.org>
2013-12-03 16:07:23 +1100

    Move the servlet configuration to annotations

--- src/main/java/footprints/server/ManifestServlet.java
+++ src/main/java/footprints/server/ManifestServlet.java
@@ -1,8 +1,10 @@
 package footprints.server;
 
+import javax.servlet.annotation.WebServlet;
 import org.realityforge.gwt.appcache.server.AbstractManifestServlet;
 import org.realityforge.gwt.appcache.server.propertyprovider.UserAgentPropertyProvider;
 
+@WebServlet( urlPatterns = { "/footprints.manifest" } )
 public class ManifestServlet
   extends AbstractManifestServlet
 {
